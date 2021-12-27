8ff79b1d45fce4109ca18fbaca5e8fd1240b25ad
millserik <81333087+millserik@users.noreply.github.com>
2021-12-26 19:18:42 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,6 +4,9 @@ function setup() {
     [0,0,0,0],
     [0,0,0,0]
   ];
+  time = 0;
+  mins = 0;
+
 }
 
 function draw() {
@@ -12,15 +15,35 @@ function draw() {
   noStroke()
   fill(3,190,252)
   rect(0,0,480,32)
+
+  textSize(20)
+  stroke(0)
+  fill(0)
+  text('Buttons',8,24)
+  time = time + deltaTime;
+  out_time = floor(time/1000)
+  if(out_time == 60){
+    mins = mins + 1
+    time = 0
+    out_time = 0
+  }
+  if(out_time < 10){
+    print_time = '0'+ out_time
+  } else{
+    print_time = out_time
+  }
+  text(mins + ':' + print_time,240,24)
+
+  
   stroke(255)
   
   for(let j = 0; j < 4; j++){
     for(let i = 0; i < 2; i++){
       
       if(grid[i][j] == 1){
-        fill(0,255,0)
+        fill(5,176,48)
       } else{
-        fill(255,0,0)
+        fill(184,36,26)
       }
       
       rect(j*120,i*120+32,120,120)
